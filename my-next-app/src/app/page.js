33cc666d3599292507dbdb43af219afb7fff8dc7
Abubakarshykh <sheikhabubaker498@gmail.com>
2025-03-14 00:44:23 +0500

    committed everything

--- my-next-app/src/app/page.js
+++ my-next-app/src/app/page.js
@@ -1,22 +1,32 @@
-import Footer from "@/Components/Footer";
+"use client"; // Marks this as a client-side component
+
+import { useState, useEffect } from "react";
+import Preloader from "@/Components/Preloader"; 
 import Header from "@/Components/Header";
-import Navbar from "@/Components/Navbar";
-import Our_projects from "@/Components/Our_projects";
 import Services from "@/Components/Services";
 import Testimonials from "@/Components/Testimonials";
-import Image from "next/image";
+import OurProjects from "@/Components/OurProjects";
 
 export default function Home() {
+  // const [loading, setLoading] = useState(true);
+
+  // useEffect(() => {
+  //   const timer = setTimeout(() => setLoading(false), 3000); // Preloader for 3 sec
+  //   return () => clearTimeout(timer);
+  // }, []);
+
   return (
     <>
-    <div className="bg-[#222222] ">
-    <Navbar/>
-    <Header/>
-    <Services/>
-    <Testimonials/>
-    <Our_projects/>
-    <Footer/>
-    </div>
+      {/* {loading ? (
+        <Preloader />
+      ) : ( */}
+        <div className="bg-[#222222]">
+          <Header />
+          <Services />
+          <Testimonials />
+          <OurProjects />
+        </div> 
+      {/* )} */}
     </>
   );
 }
